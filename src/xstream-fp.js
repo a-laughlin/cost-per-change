ce77e83d631cdff76ddd02042afc3077e48fb2b6
Adam Laughlin <adam.laughlin@gmail.com>
2018-05-07 20:23:21 -0600

    sorted xstream properties alphabetically

--- src/xstream-fp.js
+++ src/xstream-fp.js
@@ -28,26 +28,26 @@ console.log(`xstream`, xstream);
 // which is which
 // operators
 export const addListener = (...a)=>s=>s.addListener(...a);
-export const removeListener = (...a)=>s=>s.removeListener(...a);
-export const subscribe = (...a)=>s=>s.subscribe(...a);
+export const debug = (...a)=>s=>s.debug(...a);
+export const drop = (...a)=>s=>s.drop(...a);
+export const endWhen = (...a)=>s=>s.endWhen(...a);
+export const filter = (...a)=>s=>s.filter(...a);
+export const flatten = (...a)=>s=>s.flatten(...a);
+export const fold = (...a)=>s=>s.fold(...a); // - returns MemoryStream
+export const imitate = (...a)=>s=>s.imitate(...a); // imitates any non-Memory Stream
+export const last = (...a)=>s=>s.last(...a);
 export const map = (...a)=>s=>s.map(...a);
 export const mapTo = (...a)=>s=>s.mapTo(...a);
-export const filter = (...a)=>s=>s.filter(...a);
-export const take = (...a)=>s=>s.take(...a);
-export const drop = (...a)=>s=>s.drop(...a);
-export const last = (...a)=>s=>s.last(...a);
-export const startWith = (...a)=>s=>s.startWith(...a); // - returns MemoryStream
-export const endWhen = (...a)=>s=>s.endWhen(...a);
-export const fold = (...a)=>s=>s.fold(...a); // - returns MemoryStream
-export const replaceError = (...a)=>s=>s.replaceError(...a);
-export const flatten = (...a)=>s=>s.flatten(...a);
 export const remember = (...a)=>s=>s.remember(...a);
-export const debug = (...a)=>s=>s.debug(...a);
-export const imitate = (...a)=>s=>s.imitate(...a); // imitates any non-Memory Stream
-export const shamefullySendNext = (...a)=>s=>s.shamefullySendNext(...a);
-export const shamefullySendError = (...a)=>s=>s.shamefullySendError(...a);
-export const shamefullySendComplete = (...a)=>s=>s.shamefullySendComplete(...a);
+export const removeListener = (...a)=>s=>s.removeListener(...a);
+export const replaceError = (...a)=>s=>s.replaceError(...a);
 export const setDebugListener = (...a)=>s=>s.setDebugListener(...a);
+export const shamefullySendComplete = (...a)=>s=>s.shamefullySendComplete(...a);
+export const shamefullySendError = (...a)=>s=>s.shamefullySendError(...a);
+export const shamefullySendNext = (...a)=>s=>s.shamefullySendNext(...a);
+export const startWith = (...a)=>s=>s.startWith(...a); // - returns MemoryStream
+export const subscribe = (...a)=>s=>s.subscribe(...a);
+export const take = (...a)=>s=>s.take(...a);
 
 // extras
 export {
